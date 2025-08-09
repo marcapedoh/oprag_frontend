8022fe7a7132938be7c366d3e0987cd7f8ab10e3
marcapedoh <adralabel@gmail.com>
2025-08-09 14:45:27 +0200

    fix validite error

--- src/app/components/badge-list/badge-list.component.ts
+++ src/app/components/badge-list/badge-list.component.ts
@@ -19,12 +19,40 @@ export class BadgeListComponent implements OnInit {
     columnsName: ["id", "numero", "validite", "numeroParc", "inspecteur.nom"],
     field: ["#", "numero", "validite", "Numero Parc", "NomInspecteur"]
   }
+  moisOptions = [
+    { value: 'UN_MOIS', label: '1 MOIS' },
+    { value: 'DEUX_MOIS', label: '2 MOIS' },
+    { value: 'TROIS_MOIS', label: '3 MOIS' },
+    { value: 'QUATRE_MOIS', label: '4 MOIS' },
+    { value: 'CINQ_MOIS', label: '5 MOIS' },
+    { value: 'SIX_MOIS', label: '6 MOIS' },
+    { value: 'SEPT_MOIS', label: '7 MOIS' },
+    { value: 'HUIT_MOIS', label: '8 MOIS' },
+    { value: 'NEUF_MOIS', label: '9 MOIS' },
+    { value: 'DIX_MOIS', label: '10 MOIS' },
+    { value: 'ONZE_MOIS', label: '11 MOIS' },
+    { value: 'DOUZE_MOIS', label: '12 MOIS' },
+    { value: 'TREIZE_MOIS', label: '13 MOIS' },
+    { value: 'QUATORZE_MOIS', label: '14 MOIS' },
+    { value: 'QUINZE_MOIS', label: '15 MOIS' },
+    { value: 'SEIZE_MOIS', label: '16 MOIS' },
+    { value: 'DIX_SEPT_MOIS', label: '17 MOIS' },
+    { value: 'DIX_HUIT_MOIS', label: '18 MOIS' },
+    { value: 'DIX_NEUF_MOIS', label: '19 MOIS' },
+    { value: 'VINGT_MOIS', label: '20 MOIS' }
+  ];
   badges = []
   ngOnInit(): void {
     this.badges$.subscribe((badges: any) => {
       console.log(badges.badges)
       if (Array.isArray(badges.badges) && badges.badges.length > 0) {
-        this.badges = badges.badges
+        this.badges = badges.badges.map((badge: any) => {
+          const option = this.moisOptions.find(opt => opt.value === badge.validite);
+          return {
+            ...badge,
+            validite: option ? option.label : badge.validite
+          };
+        });
       } else {
         console.log('No badges found or still loading.');
       }
--- src/app/components/inspections-vehicule-list/inspections-vehicule-list.component.ts
+++ src/app/components/inspections-vehicule-list/inspections-vehicule-list.component.ts
@@ -21,13 +21,41 @@ export class InspectionsVehiculeListComponent implements OnInit {
       columnsName: ["id", "creationDate", "societe", "numeroRapport", "localisationCertificationFait", "description", "validite", "utilisateur.nom"],
       field: ["#", "Date Creation", "Societe", "Numero Rapport", "Localisation", "Description", "validite", "Inspecteur"]
     }
+  moisOptions = [
+    { value: 'UN_MOIS', label: '1 MOIS' },
+    { value: 'DEUX_MOIS', label: '2 MOIS' },
+    { value: 'TROIS_MOIS', label: '3 MOIS' },
+    { value: 'QUATRE_MOIS', label: '4 MOIS' },
+    { value: 'CINQ_MOIS', label: '5 MOIS' },
+    { value: 'SIX_MOIS', label: '6 MOIS' },
+    { value: 'SEPT_MOIS', label: '7 MOIS' },
+    { value: 'HUIT_MOIS', label: '8 MOIS' },
+    { value: 'NEUF_MOIS', label: '9 MOIS' },
+    { value: 'DIX_MOIS', label: '10 MOIS' },
+    { value: 'ONZE_MOIS', label: '11 MOIS' },
+    { value: 'DOUZE_MOIS', label: '12 MOIS' },
+    { value: 'TREIZE_MOIS', label: '13 MOIS' },
+    { value: 'QUATORZE_MOIS', label: '14 MOIS' },
+    { value: 'QUINZE_MOIS', label: '15 MOIS' },
+    { value: 'SEIZE_MOIS', label: '16 MOIS' },
+    { value: 'DIX_SEPT_MOIS', label: '17 MOIS' },
+    { value: 'DIX_HUIT_MOIS', label: '18 MOIS' },
+    { value: 'DIX_NEUF_MOIS', label: '19 MOIS' },
+    { value: 'VINGT_MOIS', label: '20 MOIS' }
+  ];
   certificatControls = []
   ngOnInit(): void {
 
     this.inpectionsVehicule$.subscribe((certificatControl: any) => {
       console.log(certificatControl.certificatControls)
       if (Array.isArray(certificatControl.certificatControls) && certificatControl.certificatControls.length > 0) {
-        this.certificatControls = certificatControl.certificatControls
+        this.certificatControls = certificatControl.certificatControls.map((ctrl: any) => {
+          const option = this.moisOptions.find(opt => opt.value === ctrl.validite);
+          return {
+            ...ctrl,
+            validite: option ? option.label : ctrl.validite // si trouvé, remplace par le label
+          };
+        });
       } else {
         console.log('No certificatControl found or still loading.');
       }
